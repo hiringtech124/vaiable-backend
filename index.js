4ad6f6ca1e1692bb7940a9b8d48fcaf55868b386
sudhanshu30602 <sudhanshugupta30602@gmail.com>
2024-05-28 13:48:23 +0530

    changes

--- index.js
+++ index.js
@@ -4,24 +4,23 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const User = require("./model/User");
 const authRoutes = require("./routes/auth");
-const chatRoutes = require("./routes/chatRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const admin = require("firebase-admin");
 const http = require('http');
-const socketIo = require('socket.io');
+//const socketIo = require('socket.io');
 
 
 
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+//const io = socketIo(server);
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: false, limit:10000, parameterLimit:3,}));
 app.use(authRoutes);
-//app.use(chatRoutes);
+
 app.use(taskRoutes);
 
 
@@ -31,34 +30,34 @@ app.get('/', (req, res) => {
 
 
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
+// io.on('connection', (socket) => {
+//   console.log('New client connected');
 
-  socket.on('joinRoom', ({ userId }) => {
-    socket.join(userId);
-  });
+//   socket.on('joinRoom', ({ userId }) => {
+//     socket.join(userId);
+//   });
 
-  socket.on('sendMessage', async ({ userId, recipientId, text }) => {
-    const newMessage = {
-      senderId: userId,
-      recipientId,
-      text,
-      timestamp: admin.firestore.FieldValue.serverTimestamp()
-    };
+//   socket.on('sendMessage', async ({ userId, recipientId, text }) => {
+//     const newMessage = {
+//       senderId: userId,
+//       recipientId,
+//       text,
+//       timestamp: admin.firestore.FieldValue.serverTimestamp()
+//     };
 
-    try {
-      await db.collection('users').doc(userId).collection('messages').add(newMessage);
-      await db.collection('users').doc(recipientId).collection('messages').add(newMessage);
-      io.to(recipientId).emit('receiveMessage', newMessage);
-    } catch (error) {
-      console.error('Error sending message: ', error);
-    }
-  });
+//     try {
+//       await db.collection('users').doc(userId).collection('messages').add(newMessage);
+//       await db.collection('users').doc(recipientId).collection('messages').add(newMessage);
+//       io.to(recipientId).emit('receiveMessage', newMessage);
+//     } catch (error) {
+//       console.error('Error sending message: ', error);
+//     }
+//   });
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
+//   socket.on('disconnect', () => {
+//     console.log('Client disconnected');
+//   });
+// });
 
 
 const port = process.env.PORT || 3000;
